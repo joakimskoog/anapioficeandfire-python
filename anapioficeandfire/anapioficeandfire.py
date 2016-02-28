4a92d3001919f5c261f7640c37f4990b33ab9f84
Joakim Skoog <glasvegas@hotmail.se>
2016-02-28 18:33:30 +0100

    Created definitions for getting all entities

--- anapioficeandfire/anapioficeandfire.py
+++ anapioficeandfire/anapioficeandfire.py
@@ -23,16 +23,28 @@ def _get(id, model_type):
     return result
 
 
+def get_all_books(name, from_release_date, to_release_date):
+    return None
+
+
 def get_book(book_id):
     ''' Return a single book '''
     result = _get(book_id, 'books')
     return Book(result.content)
 
 
+def get_all_characters(name, culture, born, died, is_alive):
+    return None
+
+
 def get_character(character_id):
     ''' Return a single character '''
     result = _get(character_id, 'characters')
     return Character(result.content)
+
+
+def get_all_houses(name, region, words, has_words, has_titles, has_seats, has_died_out, has_ancestral_weapons):
+    return None
 
 
 def get_house(house_id):
