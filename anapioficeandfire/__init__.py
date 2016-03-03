9a64821a7ab79f3c8f52657f2983957baea799a8
Joakim Skoog <glasvegas@hotmail.se>
2016-03-03 18:35:24 +0100

    Importing submodules in init

--- anapioficeandfire/__init__.py
+++ anapioficeandfire/__init__.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from anapioficeandfire.models import Book, Character, House, ModelFactory
+from anapioficeandfire.error import AnApiOfIceAndFireError
+from anapioficeandfire.api import API
+from anapioficeandfire.cursor import Cursor
+
 __author__ = 'Joakim Skoog'
 __email__ = 'joakimskooog@gmail.com'
 __version__ = '0.1.0'
